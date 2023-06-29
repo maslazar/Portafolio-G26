137d7dfd7bd3c7927d1a68cb404cf216b5f254f9
Manuela <maslazar1995@gmail.com>
2023-06-28 21:11:31 -0500

    terminación 101%

--- src/componets/content.jsx
+++ src/componets/content.jsx
@@ -24,19 +24,19 @@ function Content() {
                     <div className="sm:w-1/1 md:w-1/3 lg:w-1/3 p-[10px]">
                         <p className="text-[100px]">01</p>
                         <p className="text-[20px] text-[#8def8d]">Rick and Morty</p>
-                        <p>En este proyecto veremos en que galaxia vive cada uno de los personajes de Rick and Morty.</p>
+                        <p>Este proyecto se realizo con el propósito de crear usuarios con implementación de modal. El desafió que se me presento en este proyecto es la lógica de paginación por bloques con el uso de tegnología como React, TailwindCSS, JavaScript.</p>
                         <a target='_blank' rel="noreferrer" href="https://github.com/maslazar/GEN-26RickandMorty">LINK</a>
                     </div>
                     <div className="sm:w-1/1 md:w-1/3 lg:w-1/3 p-[10px]">
                         <p className="text-[100px]">02</p>
-                        <p className="text-[20px] text-[#ffff78]">Pokemon</p>
-                        <p>Nos ´permite buscar pokemones y ver toda su información.</p>
+                        <p className="text-[20px] text-[#ffff78]">Pokedex</p>
+                        <p>Este proyecto se realizo con el propósito de crear rutas, una configuración de estado global, maqueado y funcionalidad de home. El desafió que se me presento en este proyecto es la lógica de paginación por bloques con el uso de tegnología como React, TailwindCSS, JavaScript.</p>
                         <a target='_blank' rel="noreferrer" href="https://github.com/maslazar/Pokemon-G26">LINK</a>
                     </div>
                     <div className="sm:w-1/1 md:w-1/3 lg:w-1/3 p-[10px]">
                         <p className="text-[100px]">03</p>
-                        <p className="text-[20px] text-[#9a0000]">Carrito de compras</p>
-                        <p>sistema con funcion de carrito de compras para una licorera.</p>
+                        <p className="text-[20px] text-[#ed8181]">Carrito de compras</p>
+                        <p>Este proyecto se realizo con el propósito de crear la lógica carrito de compras con las propiedades del DOM y eventos. El desafió que se me presento en este proyecto fue el redireccionamiento de productos con el uso de tegnología como DOM, JavaScript  .</p>
                         <a target='_blank' rel="noreferrer" href="https://github.com/maslazar/LicorStore">LINK</a>
                     </div>
 
