04b501a1650bb5d21a8cc6a3da91fffdaaf70735
Manuela <maslazar1995@gmail.com>
2023-06-28 20:28:22 -0500

    links menu

--- src/componets/content.jsx
+++ src/componets/content.jsx
@@ -9,14 +9,14 @@ function Content() {
                     <div className="justify-center flex bg-[#181823] w-full sm:w-1/1 md:w-1/2 lg:w-1/2 ">
                         <img className="p-[40px] rounded-[59px] h-[400px]" src="./src/assets/icons/Foto.jpeg" alt="" />
                     </div>
-                    <div className="block mt-auto mb-auto text-center sm:w-1/1 md:w-1/2 lg:w-1/2">
+                    <div id="sobremi" className="block mt-auto mb-auto text-center sm:w-1/1 md:w-1/2 lg:w-1/2">
                         <h1 className="font-bold mt-[20px]">Mi nombre es Manuela Salazar </h1>
                         <h2>soy de colombia</h2>
                         <h3>soy profecional en Mercadeo y Publicidad , actualmente estudiante de programación, en el paso de mi vida laborar e desarrollado buenas habilidades de colaboración dentro de un equipo productivo</h3>
                     </div>
                 </div>
 
-                <div className=" flex flex-wrap">
+                <div id="proyectos" className=" flex flex-wrap">
                     <div className="mt-[20px] text-[20px] font-bold w-full sm:w-1/1 md:w-1/1 lg:w-1/1">
                         <h2>Proyectos</h2>
                     </div>
@@ -42,7 +42,7 @@ function Content() {
 
                 </div>
 
-                <div className="mt-[30px]">
+                <div id="contacto" className="mt-[30px]">
                     <div>
                         <h2 className="text-[20px] font-bold">contacto</h2>
                         <a target='_blank' rel="noreferrer" href="https://api.whatsapp.com/send/?phone=%2B573158111531&text&type=phone_number&app_absent=0" ><img src="./src/assets/icons/phone.png" alt="" /></a>
--- src/componets/navbar.jsx
+++ src/componets/navbar.jsx
@@ -16,10 +16,9 @@ function Navbar() {
 
         <div className={`lg:block md:block ml-auto pr-[20px] bg-[#181823] ${value} ` + (value ? 'block' : 'hidden')}>
           <div className={"navbar " + (value ? 'inline-grid' : 'flex' )}>
-            <a className="text-white hover:text-[#5221E6]" href="#">Inico</a>
-            <a className="text-white hover:text-[#5221E6]" href="#">Sobre mi</a>
-            <a className="text-white hover:text-[#5221E6]" href="#">proyectos</a>
-            <a className="text-white hover:text-[#5221E6]" href="#">contacto</a>
+            <a className="text-white hover:text-[#5221E6]" href="#sobremi">Sobre mi</a>
+            <a className="text-white hover:text-[#5221E6]" href="#proyectos">proyectos</a>
+            <a className="text-white hover:text-[#5221E6]" href="#contacto">contacto</a>
           </div>
 
         </div>
